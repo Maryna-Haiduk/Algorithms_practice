24f9c74ab147be0340d3a9f0b14ff8e4efd2dbc4
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-07-24 17:16:33 -0400

    Added SelectionSort_Implementation

---
+++ Algorithms_Practice/SelectionSort_Implementation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class SelectionSort_Implementation
+    {
+        public void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                    int temp = array[i];
+                    array[i] = array[minIndex];
+                    array[minIndex] = temp;
+                }
+            }
+        }
+    }
+}
