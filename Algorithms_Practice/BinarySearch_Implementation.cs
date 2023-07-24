594842f105d61aa1c897123741631f0ed56f56dd
Maryna Haiduk <136976257+Maryna-Haiduk@users.noreply.github.com>
2023-07-24 17:14:41 -0400

    Added BinarySearch_Implementation

---
+++ Algorithms_Practice/BinarySearch_Implementation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class BinarySearch_Implementation
+    {
+        private int BinarySearch(int[] array, int item)
+        {
+            Array.Sort(array);
+
+            int left = 0;
+            int right = array.Length - 1;
+
+            if (array[left] == item)
+            { return left; }
+            else if (array[right] == item)
+            { return right; }
+
+            while (left <= right)
+            {
+                var midle = (left + right) / 2;
+
+                if (array[midle] == item)
+                { return midle; }
+
+                if (item < array[midle])
+                {
+                    right = midle - 1;
+                }
+                else if (item > array[midle])
+                {
+                    left = midle + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
